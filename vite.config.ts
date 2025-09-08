f92c8a07cc7c0f29bdcfc3f9d5aab8dfd6d85a37
Arfana Ali <arfanaali07@gmail.com>
2025-09-08 15:25:58 +0530

    Project is ready to update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { vercelPreset } from "@vercel/remix/vite";
+import dotenv from "dotenv";
+dotenv.config();
+
 
 declare module "@remix-run/node" {
   interface Future {
@@ -22,7 +24,6 @@ export default defineConfig({
         v3_singleFetch: true,
         v3_lazyRouteDiscovery: true,
       },
-      presets: [vercelPreset()],
     }),
     tsconfigPaths(),
   ],
